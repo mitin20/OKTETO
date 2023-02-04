ce01a184ddfc7539f702550bcb6938f07a6776ac
mitin20 <48494797+mitin20@users.noreply.github.com>
2023-02-04 00:06:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
-import requests
- 
+#Python web app
+
+import json
+import flask
+from flask import Flask 
+
 app = Flask(__name__)
 
-# Home/Index route
-@app.route('/')
-def index():
-    response = requests.get("https://www.reddit.com/r/meme/top/.json?limit=1")
-    memeData = response.json()['data']['children'][0]['data']
-    return render_template('index.html', memeData=memeData)
- 
+@app.route('/memes/api')
+def get_memes():
+    url = "https://www.reddit.com/r/memes/top.json?t=week"
+    response = requests.get(url, headers={'User-agent': 'your bot 0.1'})
+    if response.status_code == 200:
+        data = response.json()
+        memes = []
+
+        for item in data['data']['children']:
+            meme = item['data']
+            memes.append(meme)
+
+        return json.dumps(memes)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
