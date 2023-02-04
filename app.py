1f89a4f47552f63f7f97b5191f595019a8e43999
mitin20 <48494797+mitin20@users.noreply.github.com>
2023-02-04 13:42:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,25 @@ def get_memes():
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run(host="0.0.0.0", port=5000)
+
+
+#meme-app-2.0
+from flask import Flask, render_template
+import request
+import json
+
+def get_meme():
+#Uncomment these two lines and comment out the other url line if yo
+# sr = "/wholesomememes"
+# url = "https://meme-api.herokuapp.com/gimme" + sr
+url = "https://meme-api.herokuapp.com/gimme"
+response = json.loads (requests.request("GET", url).text)
+meme_large = response["preview"][-2]
+subreddit = response["subreddit"]
+return meme_large, subreddit
+@app.route("/")
+def index():
+meme_pic,subreddit = get_meme()
+return render_ template("meme_index.html"
+meme_pic=meme_pic, subreddit=subreddit)
+app.run (host="0.0.0.0", port=80)_
